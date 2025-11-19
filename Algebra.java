c0f8f3fb0c5f76bd868a7be15e431320272e9b94
ori solan <ori.solanstewart@post.runi.ac.il>
2025-11-19 19:14:16 +0200

    algebra4

--- Algebra.java
+++ Algebra.java
@@ -92,8 +92,7 @@ public class Algebra {
 	}
 
 	public static int times(int x1, int x2) {
-		public static int times(int x1, int x2) {
-    
+		
     if (x1 == 0 || x2 == 0) {
         return 0;
     }
@@ -143,7 +142,21 @@ public class Algebra {
     
     return res; 
 }
-
+public static int pow(int x, int n) {
+	if (n == 0) {
+		return 1;
+	}
+	if (x == 0) {
+		return 0;	
+	}	
+	int res = 1;
+	int i = 0;
+	while (i < n) {
+		res = times(res, x);
+		i++;
+		}
+		return res;
+	}
 
 	public static int div(int x1, int x2) {
 		if (x2 == 0) {
