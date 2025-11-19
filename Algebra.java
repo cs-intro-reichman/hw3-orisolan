a458f2c10557a34725b89f2f31888e31e0de0990
ori solan <ori.solanstewart@post.runi.ac.il>
2025-11-19 16:31:29 +0200

    algebra

--- Algebra.java
+++ Algebra.java
@@ -1,67 +1,209 @@
-// Implements algebraic operations and the square root function without using 
-// the Java operations a + b, a - b, a * b, a / b, a % b, and without calling 
-// Math.sqrt. All the functions in this class operate on int values and
-// return int values.
-
 public class Algebra {
 	public static void main(String args[]) {
-	    // Tests some of the operations
-	    System.out.println(plus(2,3));   // 2 + 3
-	    System.out.println(minus(7,2));  // 7 - 2
-   		System.out.println(minus(2,7));  // 2 - 7
- 		System.out.println(times(3,4));  // 3 * 4
-   		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
-   		System.out.println(pow(5,3));      // 5^3
-   		System.out.println(pow(3,5));      // 3^5
-   		System.out.println(div(12,3));   // 12 / 3    
-   		System.out.println(div(5,5));    // 5 / 5  
-   		System.out.println(div(25,7));   // 25 / 7
-   		System.out.println(mod(25,7));   // 25 % 7
-   		System.out.println(mod(120,6));  // 120 % 6    
+	    System.out.println(plus(2,3)); 
+	    System.out.println(minus(7,2));
+   		System.out.println(minus(2,7));
+ 		System.out.println(times(3,4));
+   		System.out.println(plus(2,times(4,2)));
+   		System.out.println(pow(5,3));
+   		System.out.println(pow(3,5));
+   		System.out.println(div(12,3));   
+   		System.out.println(div(5,5));
+   		System.out.println(div(25,7));
+   		System.out.println(mod(25,7));
+   		System.out.println(mod(120,6));   
    		System.out.println(sqrt(36));
 		System.out.println(sqrt(263169));
    		System.out.println(sqrt(76123));
 	}  
 
-	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		if (x1 == 0) {
+			return x2;
+		}
+		if (x2 == 0) {
+			return x1;
+		}
+		int i = 0;
+		if (x2 < 0 && x1 < 0) {
+		while (i > x2) {
+			x1--;
+			i--;
+		}
+		}
+		if (x2 > 0 && x1 > 0) {
+		while (i < x2) {
+			x1++;
+			i++;
+		}
+		}
+		if (x2 > 0 && x1 < 0) {
+		while (i < x2) {
+			x1++;
+			i++;
+		}
+		}
+		if (x2 < 0 && x1 > 0) {
+		while (i > x2 ) {
+			x1--;
+			i--;
+		}
+		}
+
+
+		return x1;
 	}
 
-	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+	if (x1 == 0) {
+			return x2;
+		}
+		if (x2 == 0) {
+			return x1;
+		}
+		int i = 0;
+		if (x2 < 0 && x1 < 0) {
+		while (i > x2) {
+			x1++;
+			i--;
+		}
+		}
+		if (x2 > 0 && x1 > 0) {
+		while (i < x2) {
+			x1--;
+			i++;
+		}
+		}
+		if (x2 > 0 && x1 < 0) {
+		while (i < x2) {
+			x1--;
+			i++;
+		}
+		}
+		if (x2 < 0 && x1 > 0) {
+		while (i > x2 ) {
+			x1++;
+			i--;
+		}
+		}
+
+
+		return x1;
 	}
 
-	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		if (x1 == 0 || x2 == 0) {
+			return 0;
+		}
+		int i = 0;
+		int res = 0;
+		if ( x1 > 0 && x2 > 0) {
+		while ( i < x2) {
+		res = plus(x1,res);	
+		i++;
+		}
+	}
+	if ( x1 < 0 && x2 < 0) {
+	while ( i >= x2) {
+		res = plus(x1,res);	
+		i++;	
+		}
+	}
+	if ( x1 < 0 && x2 > 0) {
+	while ( i < x2) {
+		res = plus(x1,res);	
+		i++;	
+		}
+	}
+	else {
+	while ( i < x1) {
+		res = plus(x2,res);	
+		i++;	
+		}
+	}
+		return res;
 	}
 
-	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
+	if (n == 0) {
+		return 1;
 	}
-
-	// Returns the integer part of x1 / x2 
-	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
-	}
-
-	// Returns x1 % x2
-	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+	if (x == 0) {
+		return 0;	
 	}	
+	int res = 1;
+	int i = 0;
+	while (i < n) {
+		res = times(res, x);
+		i++;
+		}
+		return res;
+	}
 
-	// Returns the integer part of sqrt(x) 
-	public static int sqrt(int x) {
-		// Replace the following statement with your code
+	public static int div(int x1, int x2) {
+		if (x2 == 0) {
+			return 0;
+		}
+		if (x1 == 0) {
+			return 0;
+		}
+			boolean neg = false;
+			if (x1 < 0) {
+				x1 = minus(0, x1);
+				neg =! neg;
+			}
+			if (x2 < 0) {
+				x2 = minus(0, x2);
+				neg =! neg;
+			}
+			int count = 0;
+			while (x1 >= x2) {
+				x1 = minus(x1, x2);
+				count++;
+			}
+			if (neg) {
+                count = minus(0, count);
+			}
+		return count;
+	}
+
+	public static int mod(int x1, int x2) {
+	if (x2 == 0){
 		return 0;
+	}
+	if (x1 == 0){
+		return 0;
+	}
+	boolean negg = false;
+	if (x1 < 0) {
+		x1 = minus(0, x1);
+		negg =! negg;
+	}
+	int negx2;
+	if (x2 < 0) {
+		negx2 = minus(0, x1);
+	} else {
+			negx2 = x2;
+		}
+		while (x1 >= negx2) {
+			minus(x1, negx2);
+		}
+		if (negg) {
+			minus(0, x1);
+		}
+		return x1;
+	}
+
+	public static int sqrt(int x) {
+		if (x <= 0) {
+			return 0;
+		}
+		int num = 0;
+
+while (times(num, num) <= x) {
+	num++;
+	
+}
+		return minus(num, 1);
 	}	  	  
 }
