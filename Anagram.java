b00fa4b638fb429a1391d0c2e85a86811a3d317b
ori solan <ori.solanstewart@post.runi.ac.il>
2025-11-20 20:22:11 +0200

    anagram

--- Anagram.java
+++ Anagram.java
@@ -28,22 +28,53 @@ public class Anagram {
 
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
-		// Replace the following statement with your code
-		return false;
+		str1 = preProcess(str1);
+		str2 = preProcess(str2);
+		boolean check = str1.length() == str2.length();
+		int i =0;
+		while (0 < str1.length() && check) {
+			char l1 = str1.charAt(index: 0);
+			i = 0;
+			while (i < str2.length() && l1 != str2.charAt(i)) {
+				i++;
+			}
+			if (i < str2.length()) {
+				str1 = str1.substring(beginIndex: 1, str1.length());
+				str2 = str2.substring(beginIndex: 0, i) + str2.substring(i + 1, str2.length());
+				check = true;
+			} else {
+				check = false;
+			}
+		}
+		return check;
 	}
 	   
 	// Returns a preprocessed version of the given string: all the letter characters are converted
 	// to lower-case, and all the other characters are deleted, except for spaces, which are left
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
-		// Replace the following statement with your code
-		return "";
+		str = str.toLowerCase();
+		String newStr = "";
+		int i = 0;
+		for (i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			if (ch > 96 && ch < 123) {
+				newStr += ch;
+			}
+		}
+		return newStr;
 	} 
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
 	// characters as the given string, re-arranged in a random order. 
 	public static String randomAnagram(String str) {
-		// Replace the following statement with your code
-		return "";
+		int i = 0;
+		String nstr = "";
+		while (str.length() > 1) {
+			i = (int)  (Math.random() * str.length());
+			nstr = nstr + str.charAt(i);
+			str = str.substring(beginIndex: 0,i) + str.substring(i + 1, str.length());
+		}
+		return nstr + str;
 	}
 }
